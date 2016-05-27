186fb551820f9d8e3553e6eab576884a8f2c9908
UjiAhmadFauzi <ahmad.uji1902@gmail.com>
2016-05-27 11:08:13 +0700

    first commit

---
+++ application/views/engine/footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+                    <div class="footer-left">
+                        <span>&copy; 2014. Noric and Kreuzhev Application Development. All Rights Reserved.</span>
+                    </div>
+                </div><!--footer-->
+                
